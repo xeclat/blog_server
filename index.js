9bebfa40698d20ac64c14b2d4bfcedb15dae34a8
xeclat <akwisame@gmail.com>
2025-06-29 13:07:37 +0200

    app.js, controlles, utils, middleware refactor

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
-const express = require('express')
-const Blog = require('./models/blog')
-const app = express()
-
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  console.log('inside geeet')
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  console.log('body', request)
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
 const PORT = 3003
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${PORT}`)
 })
